0bedac22474dea3fcd4cb5155935eb09f8aaaf2d
Edwin Alexander <eabohorquezg@unal.edu.co>
2017-11-26 11:46:41 -0500

    Map, current vehicle location

--- app/src/main/java/unal/edu/co/controlcarowner/activities/CurrentLocationActivity.java
+++ app/src/main/java/unal/edu/co/controlcarowner/activities/CurrentLocationActivity.java
@@ -1,9 +1,21 @@
 package unal.edu.co.controlcarowner.activities;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,35 +28,65 @@ import unal.edu.co.controlcarowner.R;
  * Created by kevincastro on 11/26/17.
  */
 
-public class CurrentLocationActivity extends AppCompatActivity{
+public class CurrentLocationActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     private FirebaseDatabase database;
+    private SupportMapFragment mapFragment;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currentlocation);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Ubicacion actual del vehiculo");
+        }
+
         database = FirebaseDatabase.getInstance();
 
-        viewCurLocation();
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
     }
 
-    public void viewCurLocation(){
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
         DatabaseReference ref = database.getReference("Travels/" + getIntent().getExtras().getString("key")  + "/curLocation" );
         ref.addValueEventListener(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String[] location = dataSnapshot.getValue(String.class).split(",");
-                Log.d("latitud", location[0]);
-                Log.d("longitud", location[1]);
-                //Pintar el mapa aqui
+                double latitude = Double.valueOf(location[0]);
+                double longitude = Double.valueOf(location[1]);
+                LatLng place = new LatLng(latitude, longitude);
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 15));
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
         });
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
 }
